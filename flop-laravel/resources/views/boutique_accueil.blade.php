b7ba907a3a27549a1f251671998e7490159c1d6e
LeaTalon <79629352+kassandrawoods@users.noreply.github.com>
2023-06-01 15:41:17 +0200

    boutique item

--- flop-laravel/resources/views/boutique_accueil.blade.php
+++ flop-laravel/resources/views/boutique_accueil.blade.php
@@ -3,19 +3,42 @@
 <link rel="stylesheet" href="{{asset('css/boutique_accueil.css')}}">
 @endsection
 @section('content')
-<div class=FontInter header>
+<div class='FontInter header' >
+    <hr class='separation'>
     <h1 class='titre'>Bienvenu.e sur la boutique</h1>
     <div class='input-container'>
-        <div class='FontInterBlack'>
-            <h2 class='titreNombreDeColorCoins'>ColorCoins</h2>
-        </div>
+            <h2 class='titreNombreDeColorCoins black'>ColorCoins</h2>
         <img src="img/Icone-ColorCoins.png" class="ColorCoinsImage">
-        <div class="FontInterBlack">
-            <h2 class="nombreDeColorCoins">75</h2>
-        </div>
+            <h2 class="nombreDeColorCoins black">75</h2>
     </div>
-    <div class='bouton'>
         <button id='compte'>Voir mon compte</button>
+</div>
+<div class="centre">
+    <div class="column-container">
+        <div class='column-item'>
+        <div class='item'>
+            <img src="img/badges.png" class='img-item'>
+                <p>Item 1</p>
+            </div>
+        </div>
+        <div class='column-item'>
+        <div class='item'>
+            <img src="img/stickers.png" class='img-item'>
+                <p>Item 1</p>
+            </div>
+        </div>
+        <div class='column-item'>
+        <div class='item'>
+            <img src="img/casque.png" class='img-item'>
+                <p>Item 1</p>
+            </div>
+        </div>
+        <div class='column-item'>
+        <div class='item'>
+            <img src="img/casquette.png" class='img-item'>
+                <p>Item 1</p>
+            </div>
+        </div>
     </div>
 </div>
 
