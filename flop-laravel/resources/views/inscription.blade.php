50f4910e457d8bb81db44d729bdcdb087e05e21a
LeaTalon <79629352+kassandrawoods@users.noreply.github.com>
2023-06-01 10:47:03 +0200

    gestion erreurs

--- flop-laravel/resources/views/inscription.blade.php
+++ flop-laravel/resources/views/inscription.blade.php
@@ -11,20 +11,41 @@
   <div class='input-container'>
     <form method="POST" action="{{route('user.store')}}" accept-charset="UTF-8">
     @csrf
-      <input class='inscription border-placeholder' type="text" name="firstname" v-model="firstName">
-      <span class='placeholder-haut FontInter'>Prénom</span>
-      <input class='inscription border-placeholder' type="text" name="lastname" v-model="lastName">
-      <span class='placeholder-haut FontInter'>Nom</span>
-      <input class='inscription border-placeholder' type="text" name="nickname" v-model="username">
-      <span class='placeholder-haut FontInter'>Nom d'utilisateur*</span>
-      <input class='inscription border-placeholder' type="email" name="email" v-model="email">
-      <span class='placeholder-haut FontInter'>Adresse e-mail*</span>
-      <input class='inscription border-placeholder' type="tel" name="phone-number" v-model="phone">
-      <span class='placeholder-haut FontInter'>Numéro de téléphone</span>
-      <input class='inscription border-placeholder' type="password" name="password" v-model="password">
-      <span class='placeholder-haut FontInter'>Mot de passe*</span>
-      <input class='inscription border-placeholder' type="text" name="address" v-model="address">
-      <span class='placeholder-haut FontInter'>Adresse</span>
+      <div class="form-group {!! $errors->has('firstname') ? 'has-error' :'' !!}">
+        <input class='inscription border-placeholder' type="text" name="firstname" v-model="firstName">
+        <span class='placeholder-haut FontInter'>Prénom</span>
+        {!! $errors->first('firstname', '<small class="help-block">:message</small>') !!}
+      </div>
+      <div class="form-group {!! $errors->has('lastname') ? 'has-error' :'' !!}">
+        <input class='inscription border-placeholder' type="text" name="lastname" v-model="lastName">
+        <span class='placeholder-haut FontInter'>Nom</span>
+        {!! $errors->first('lastname', '<small class="help-block">:message</small>') !!}
+      </div>
+      <div class="form-group {!! $errors->has('nickname') ? 'has-error' :'' !!}">
+        <input class='inscription border-placeholder' type="text" name="nickname" v-model="username">
+        <span class='placeholder-haut FontInter'>Nom d'utilisateur*</span>
+        {!! $errors->first('nickname', '<small class="help-block">:message</small>') !!}
+      </div>
+      <div class="form-group {!! $errors->has('email') ? 'has-error' :'' !!}">
+        <input class='inscription border-placeholder' type="email" name="email" v-model="email">
+        <span class='placeholder-haut FontInter'>Adresse e-mail*</span>
+        {!! $errors->first('email', '<small class="help-block">:message</small>') !!}
+      </div>
+      <div class="form-group {!! $errors->has('phone_number') ? 'has-error' :'' !!}">
+        <input class='inscription border-placeholder' type="tel" name="phone_number" v-model="phone">
+        <span class='placeholder-haut FontInter'>Numéro de téléphone</span>
+        {!! $errors->first('phone_number', '<small class="help-block">:message</small>') !!}
+      </div>
+      <div class="form-group {!! $errors->has('password') ? 'has-error' :'' !!}">
+        <input class='inscription border-placeholder' type="password" name="password" v-model="password">
+        <span class='placeholder-haut FontInter'>Mot de passe*</span>
+        {!! $errors->first('password', '<small class="help-block">:message</small>') !!}
+      </div>
+      <div class="form-group {!! $errors->has('address') ? 'has-error' :'' !!}">
+        <input class='inscription border-placeholder' type="text" name="address" v-model="address">
+        <span class='placeholder-haut FontInter'>Adresse</span>
+        {!! $errors->first('address', '<small class="help-block">:message</small>') !!}
+    </div>
       <div class='basForm'>
         <div class='condition'>
           <label class='checkbox'>
