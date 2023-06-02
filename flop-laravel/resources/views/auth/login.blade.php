22f371879c86082cc3dd64d0891faafc0456311c
LeaTalon <79629352+kassandrawoods@users.noreply.github.com>
2023-06-02 11:21:06 +0200

    Update login.blade.php

--- flop-laravel/resources/views/auth/login.blade.php
+++ flop-laravel/resources/views/auth/login.blade.php
@@ -11,17 +11,14 @@
     <h1 class='titre'>Connexion</h1>
     <hr class='separation'>
     <div class='input-container'>
-        <div class="card-header">{{ __('Login') }}</div>
             <form method="POST" action="{{ route('login') }}">
                 @csrf
-                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
                 <input id="email" type="email" class="connexion border-placeholder form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                 <span class='placeholder-haut FontInter'>E-mail</span>
                 @error('email')
                 <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong></span>
                                 @enderror
-                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
                 <input id="password" type="password" class="connexion border-placeholder form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                 <span class='placeholder-haut FontInter'>Mot de passe</span>
                 @error('password')
