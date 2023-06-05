11a01ee90d48e0f68c4683a99df0cf0b1257ed82
Jovan Stojanovic <jovan.stojanovic.1008@gmail.com>
2023-06-05 10:39:15 +0200

    chatv1

--- flop-laravel/resources/js/app.js
+++ flop-laravel/resources/js/app.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 
 import App from "./components/Countdown.vue";
+import Chat from "./components/Chat.vue";
 
 const myApp = createApp(App);
+const myChat = createApp(Chat);
 
 myApp.mount("#app");
+myChat.mount("#chat");
