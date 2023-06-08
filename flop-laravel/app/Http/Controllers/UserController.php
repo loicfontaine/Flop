a9195dff5b31786618c3f6235ba88ced8ac22acf
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-08 15:39:39 +0200

    return dashboard

--- flop-laravel/app/Http/Controllers/UserController.php
+++ flop-laravel/app/Http/Controllers/UserController.php
@@ -70,9 +70,9 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        dd($request->all(), $id);
         User::findOrFail($id)->update($request->all());
-        //return view('dashboard')->withOk("L'utilisateur " . $request->input('name') . " a été modifié");
+        // return view('dashboard')->withOk("L'utilisateur " . $request->input('name') . " a été modifié");
+        return view('dashboard');
     }
 
     /**
