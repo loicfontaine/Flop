21baff3e8f5e91e8b82868ed612b88fb65a00afd
Rico <ricprt2000@gmail.com>
2023-06-08 11:58:16 +0200

    dsa

--- flop-laravel/resources/views/admin_dashboard.blade.php
+++ flop-laravel/resources/views/admin_dashboard.blade.php
@@ -337,7 +337,7 @@
     nouvelInput.type = 'text';
     nouvelInput.name = 'options[]';
     nouvelInput.placeholder = 'Option';
-    nouvelInput.classList.add('form optionForm');
+    nouvelInput.classList.add('form').add('optionForm');
 
     // Ajouter le nouvel élément au conteneur d'options
     conteneurOptions.appendChild(nouvelInput);
