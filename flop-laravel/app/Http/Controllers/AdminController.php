03dc5f670eee055d71ddf53cadd8155dc9ecc544
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-08 10:10:31 +0200

    issue admincontroller

---
+++ flop-laravel/app/Http/Controllers/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => 'index']);
+        $this->middleware('admin', ['only' => 'destroy']);
+    }
+
+
+    public function dashboard()
+    {
+        //si authentifier et admin return dashboard
+        //sinon return accueil
+
+        dd(auth()->user()->is_admin);
+        if (auth()->user()->is_admin) {
+            $articles = Article::all();
+            return view("admin_dashboard", compact("articles"));
+        } else {
+            return view("/");
+        }
+    }
+}
