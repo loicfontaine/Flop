724fd5ba13f41e7325384ef7765970e59d282540
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-08 10:50:46 +0200

    getInfo

--- flop-laravel/app/Http/Controllers/HomeController.php
+++ flop-laravel/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Challenge;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,6 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
     }
 
     /**
@@ -24,5 +24,13 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function getInfo()
+    {
+        $challenges = Challenge::where("end_time", ">", date("Y-m-d H:i:s"))->get();
+        $types = $challenges->participation_types;
+
+        return view("homePage", compact("challenges, types"));
     }
 }
