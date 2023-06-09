8326761d7c2aaa4b3eab434e2e87335049f465ef
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-09 10:17:52 +0200

    final correction for challenge id in reward

--- flop-laravel/app/Http/Controllers/ChallengeController.php
+++ flop-laravel/app/Http/Controllers/ChallengeController.php
@@ -82,7 +82,7 @@ class ChallengeController extends Controller
                         [
                             "quantity" => $request->input("quantity-" . $article->id),
                             "article_id" => $article->id,
-                            "challenge_id" => 1,
+                            "challenge_id" => $challenge->id,
                         ]
                     );
                 }
