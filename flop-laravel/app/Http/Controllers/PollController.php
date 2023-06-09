688bf123354de001fe39454850d5f70231905e0a
quinteirosm <quinteiros.m@gmail.com>
2023-06-09 10:43:57 +0200

    createMusic

--- flop-laravel/app/Http/Controllers/PollController.php
+++ flop-laravel/app/Http/Controllers/PollController.php
@@ -99,10 +99,10 @@ class PollController extends Controller
      */
     public function show(string $id)
     {
-        $poll = DB::table('polls')->where('id', $id)->first();
+        $sondage = DB::table('polls')->where('id', $id)->first();
         $reponses = DB::table('options')->where('poll_id', $id)->get();
 
-        return view('pollList')->with(compact('poll', 'reponses'));
+        return view('pollList')->with(compact('sondage', 'reponses'));
     }
 
     /**
