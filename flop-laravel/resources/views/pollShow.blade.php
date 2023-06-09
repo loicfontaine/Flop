677662a1bfdd2ec4796825afd41d3c2b60086f59
quinteirosm <quinteiros.m@gmail.com>
2023-06-09 11:24:25 +0200

    fixcreateMusic

---
+++ flop-laravel/resources/views/pollShow.blade.php
@@ -0,0 +1,16 @@
+@foreach ($polls as $poll)
+    <form method="POST" action="{{route('poll.destroy', $poll->id)}}" accept-charset="UTF-8">
+        @csrf
+    <p>{{$poll->title}}</p>
+    <p>{{$poll->description}}</p>
+    <br>
+    @foreach ($reponses as $index => $reponse)
+    <label>
+        <input type="radio" name="option" value="{{ $index }}">
+        {{ $reponse->title }}
+    </label>
+    <br>
+@endforeach
+    <button type="submit">Supprimer</button>
+    </form>
+@endforeach
