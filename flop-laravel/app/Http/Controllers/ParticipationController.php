cbc4b54a525b3ad4928fe74e85c93ea1ca3b9d84
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-12 09:49:36 +0200

    change method name

--- flop-laravel/app/Http/Controllers/ParticipationController.php
+++ flop-laravel/app/Http/Controllers/ParticipationController.php
@@ -27,7 +27,7 @@ class ParticipationController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function participate(Request $request)
+    public function store(Request $request)
     {
 
         dd("test");
