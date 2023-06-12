45bcbd6782f916d893e2934380f5e4049f00fac6
quinteirosm <quinteiros.m@gmail.com>
2023-06-12 22:09:21 +0200

    chat

--- flop-laravel/routes/web.php
+++ flop-laravel/routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\FileController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ChallengeController;
 use App\Models\Participation;
+use App\Models\ChatsController;
 
 /*
 |--------------------------------------------------------------------------
