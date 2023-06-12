2979a5faed27ac379424c18f2729d14ad36ba681
quinteirosm <quinteiros.m@gmail.com>
2023-06-12 23:15:32 +0200

    chatVueFix

--- flop-laravel/resources/js/components/Chat.vue
+++ flop-laravel/resources/js/components/Chat.vue
@@ -1,7 +1,25 @@
 <template>
 
 <h1 class="test">Activé (chat container)</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Chats</div>
 
+                <div class="panel-body">
+                    <chat-messages :messages="messages"></chat-messages>
+                </div>
+                <div class="panel-footer">
+                    <chat-form
+                        v-on:messagesent="addMessage"
+                        :user="{{ Auth::user() }}"
+                    ></chat-form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
