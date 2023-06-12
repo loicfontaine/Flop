667370115f64006d4fe1bbafcf59bde12c5c6f7f
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-12 15:27:49 +0200

    try stock image only

--- flop-laravel/app/Http/Controllers/FileController.php
+++ flop-laravel/app/Http/Controllers/FileController.php
@@ -13,10 +13,41 @@ class FileController extends Controller
      */
     public function formSubmit(Request $request)
     {
-
-        dd("requestAll:", $request->all(), "requestFile:", $request->file, "requestInput:", $request->input, "requestInputFile:", $request->input('file'));
-        $fileName = time() . '.' . $request->file->getClientOriginalExtension();
-        $request->file->move('/home/projart/2023/50/flop/flop-laravel/storage/participation', $fileName);
+        /*
+        dd($request->all());
+          "audio" => "undefined"
+  "textField" => "eeee"
+  "video" => "undefined"
+  "image" => Illuminate\Http\UploadedFile {#1350
+    -test: false
+    -originalName: "skype-512.png"
+    -mimeType: "image/png"
+    -error: 0
+    #hashName: null
+    path: "/tmp"
+    filename: "phpnoMAkK"
+    basename: "phpnoMAkK"
+    pathname: "/tmp/phpnoMAkK"
+    extension: ""
+    realPath: "/tmp/phpnoMAkK"
+    aTime: 2023-06-12 15:24:12
+    mTime: 2023-06-12 15:24:12
+    cTime: 2023-06-12 15:24:12
+    inode: 2248689
+    size: 1375182
+    perms: 0100600
+    owner: 33
+    group: 33
+    type: "file"
+    writable: true
+    readable: true
+    executable: false
+    file: true
+    dir: false
+    link: false
+        */
+        $fileName = time() . '.' . $request->image->getClientOriginalExtension();
+        $request->image->move('/home/projart/2023/50/flop/flop-laravel/storage/participation', $fileName);
 
         return response()->json(['success' => 'You have successfully uploaded file.']);
     }
