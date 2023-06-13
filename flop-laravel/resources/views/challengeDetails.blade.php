5328daadf2a4703de29fb99e0650eeff964fe8f1
quinteirosm <quinteiros.m@gmail.com>
2023-06-13 11:20:20 +0200

    fix divs en trop

--- flop-laravel/resources/views/challengeDetails.blade.php
+++ flop-laravel/resources/views/challengeDetails.blade.php
@@ -3,6 +3,10 @@
     <!-- Affichez d'autres détails du challenge -->
     <h3>Participations :</h3>
     @foreach ($participations as $participation)
-    1
+        <p>{{ $participation->user->name }}</p>
+        <p>{{ $participation->content->title }}</p>
+        <p>{{ $participation->content->description }}</p>
+        <p>{{ $participation->content->url }}</p>
+        <p>{{ $participation->content->type }}</p>
     @endforeach
 </div>
